aeddf7923c076ba8cf036a0553ef418814a83ea3
Takeshi Matsushita <info.itwillrain@gmail.com>
2020-01-26 22:47:59 +0900

    [env] drop console for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,55 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const path = require("path");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  devtool: "inline-source-map",
-  entry: "./src/app.ts",
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'inline-source-map',
+  entry: './src/app.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
   },
-  target: "web",
+  target: 'web',
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
       {
         test: /\.html$/,
-        loader: "html-loader"
+        loader: 'html-loader',
       },
       {
         test: /\.(s*)css$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
   },
   // import 文で .ts ファイルを解決するためa
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "index.html"
-    })
-  ]
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: 'all',
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+  },
 };
